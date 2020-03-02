974eba4042f8f9dcdf8125815f764a039dc88f9a
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2020-03-02 13:09:56 +0600

    update

--- api/post/read.php
+++ api/post/read.php
@@ -26,9 +26,31 @@
   if($num > 0) {
     // Post array 
     $post_arr = array();
-    
-  } else {
+    $post_arr['data'] = array();
 
+    while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+      extract($row);
+
+      $post_item = array(
+        'id' => $id,
+        'title' => $title,
+        'body' => html_entity_decode($body),
+        'author' => $author,
+        'category_id' => $category_id,
+        'category_name' => $category_name
+      );
+
+      // Push to "data"
+      array_push($posts_arr['data'], $post_item);
+    }
+
+    // Turn to JSON & output 
+    echo json_encode($posts_arr);
+  } else {
+    // No Posts 
+    echo json_encode(
+      array('message' => 'No Posts Found')
+    );
   }
 
 ?>
