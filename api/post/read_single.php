749964d537388395411190a62c46a2a7f0e49f84
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2020-03-12 19:44:32 +0600

    added get_single_post with id

--- api/post/read_single.php
+++ api/post/read_single.php
@@ -17,3 +17,21 @@
   $post = new Post($db);
 
   // Get ID
+  // using the ternary operator 
+  $post->id = isset($_GET['id']) ? $_GET['id'] : die();
+
+  // Get post 
+  $post->read_single();
+
+  // Create an array 
+  $post_arr = array(
+    'id' => $post->id,
+    'title' => $post->title,
+    'body' => $post->body,
+    'author' => $post->author,
+    'category_id' => $post->category_id,
+    'category_name' => $post->category_name
+  );
+
+  // Make JSON 
+  print_r(json_encode($post_arr));
